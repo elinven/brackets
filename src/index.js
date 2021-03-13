3c48ef9394cf274b15b134594d3dc36bb5360f2a
elinven <ivenhea@gmail.com>
2021-03-13 15:40:46 +0300

    feat: add js and json files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  //console.log('Check '+str);
+  let gstrlength=str.length;
+  let gfldim=true;
+  do {
+    for (let i=0;i<bracketsConfig.length;i++) {
+      //console.log('Find '+bracketsConfig[i][0]+bracketsConfig[i][1]);
+      let fldim=true;
+      let strlength=str.length;
+      do {
+        str=str.replace(bracketsConfig[i][0]+bracketsConfig[i][1],'');
+        //console.log('New '+str);
+        if ((str.length==strlength) || (str.length==0)) fldim=false;
+        strlength=str.length;
+      } while (fldim);
+    }
+    if ((str.length==gstrlength) || (str.length==0)) gfldim=false;
+    gstrlength=str.length;
+  } while (gfldim);
+  //console.log(str=='');
+  return (str=='');
 }
